6878d5bf3a6c8391196da1ebd0317f869b972393
brednan <brendanshauncaldwell@gmail.com>
2022-08-28 16:38:03 -0700

    I made a function that used the selenium Firefox webdriver to parse the page, since I need it in order to parse the page since it's dynamic.

--- account.py
+++ account.py
@@ -1,5 +1,6 @@
 from requests import Session
 from bs4 import BeautifulSoup
+from selenium.webdriver import Firefox, FirefoxProfile
 
 
 class Account(Session):
@@ -13,8 +14,8 @@ class Account(Session):
         })
 
     def get_email_address(self):
-        page = self.get('https://tempmailo.com/', timeout=5)
-        soup = BeautifulSoup(page.content, 'html.parser')
+        page = self.get_home_page()
+        soup = BeautifulSoup(page, 'html.parser')
 
         request_verification_token = soup.find('input', {'name': '__RequestVerificationToken'})['value']
 
@@ -25,7 +26,7 @@ class Account(Session):
             'requestverificationtoken': f'{request_verification_token}'
         })
 
-        email = self.get('https://tempmailo.com/changemail', timeout=5).text
+        email = soup.find('input', {'id': 'i-email'})
 
         self.email = email
 
@@ -36,8 +37,21 @@ class Account(Session):
             'mail': self.email
         }
 
-        print(self.cookies)
-
         inbox = self.post(url, data=payload, timeout=5)
 
-        print(inbox)
+    def get_home_page(self):
+        firefox_profile = FirefoxProfile()
+        firefox_profile.set_preference('general.useragent.override', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36')
+
+        driver = Firefox(firefox_profile=firefox_profile)
+
+        driver.get('https://tempmailo.com')
+
+        page_source = driver.page_source
+
+        cookies = driver.get_cookies()
+
+        for cookie in cookies:
+            self.cookies.set(cookie['name'], cookie['value'])
+
+        return page_source
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from account import Account
 
 account = Account()
 account.get_email_address()
-account.get_email_inbox()
+# account.get_email_inbox()
